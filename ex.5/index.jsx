a84ff2edfceb9b4838e0b56afac739f2ee08eda9
Alex Marçullo Teixeira Gonçalves <alex.marcullo@hotmail.com>
2017-06-28 15:41:07 -0300

    Mais uma aula

--- ex.5/index.jsx
+++ ex.5/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import  Field  from './field'
+import ClassComponent from './classComponent'
+
 
 ReactDOM.render(
-    <Field initialValue="teste" />
+    <ClassComponent initialValue={10} label="Contador"/>    
 
 , document.getElementById("app"))
